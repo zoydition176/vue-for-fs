9b7ef9f3892725c5c15c628d39f5c0f683063ddb
zoydition176 <zdx175851129@gmail.com>
2018-09-28 00:48:00 +0800

    1

--- src/js/base.js
+++ src/js/base.js
@@ -1,5 +1,5 @@
 /* 自定义js库 包含验证和弹框 */
-(function (global, factory) {
+var zbase = (function (global, factory) {
   if (typeof module === 'object' && typeof module.exports === 'object') {
     module.exports = global.document ? factory(global, true) : function (w) {
       return factory(w)
@@ -9,6 +9,7 @@
   }
 })(window, function (window, iswork) {
   var zbase = {}
+  var x = 3
   zbase.checkisInt = (number) => {
     if (number.length > 0) {
       var rg = /^[123456789]\d{0,}$/
@@ -18,7 +19,7 @@
     }
   }
   window.zbase = zbase
-  return zbase
+  return x
 })
 
 export {zbase}
