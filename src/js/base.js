e0dd5e86e6f4a8db29cee32096a7dac2658cd6b8
周迪星 <zdx175851129@gmail.com>
2018-09-28 18:42:19 +0800

    重新编辑了base.js

--- src/js/base.js
+++ src/js/base.js
@@ -1,25 +1,34 @@
 /* 自定义js库 包含验证和弹框 */
-var zbase = (function (global, factory) {
-  if (typeof module === 'object' && typeof module.exports === 'object') {
-    module.exports = global.document ? factory(global, true) : function (w) {
-      return factory(w)
-    }
-  } else {
-    factory(global)
-  }
-})(window, function (window, iswork) {
-  var zbase = {}
-  var x = 3
-  zbase.checkisInt = (number) => {
-    if (number.length > 0) {
-      var rg = /^[123456789]\d{0,}$/
-      return rg.test(number)
-    } else {
-      return false
-    }
-  }
-  window.zbase = zbase
-  return x
-})
+// var zbase = (function (global, factory) {
+//   if (typeof module === 'object' && typeof module.exports === 'object') {
+//     module.exports = global.document ? factory(global, true) : function (w) {
+//       return factory(w)
+//     }
+//   } else {
+//     factory(global)
+//   }
+// })(window, function (window, iswork) {
+//   var zbase = {}
+//   zbase.checkisInt = (number) => {
+//     if (number.length > 0) {
+//       var rg = /^[123456789]\d{0,}$/
+//       return rg.test(number)
+//     } else {
+//       return false
+//     }
+//   }
+//   window.zbase = zbase
+// })
 
+// var zbase = {
+//   checkisInt: function (number) {
+//     if (number.length > 0) {
+//       var rg = /^[123456789]\d{0,}$/
+//       return rg.test(number)
+//     } else {
+//       return false
+//     }
+//   }
+// }
+var zbase = 3
 export {zbase}
